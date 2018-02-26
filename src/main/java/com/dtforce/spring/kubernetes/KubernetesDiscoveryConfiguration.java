a93f5728f8ce7add8743ed357bb17c3b66dd1db6
Stéphane Lepin <stephane.lepin@dtforce.com>
2018-02-26 13:51:28 +0100

    Enable classpath configuration discovery for KubernetesClientConfiguration

--- src/main/java/com/dtforce/spring/kubernetes/KubernetesDiscoveryConfiguration.java
+++ src/main/java/com/dtforce/spring/kubernetes/KubernetesDiscoveryConfiguration.java
@@ -1,15 +1,17 @@
 package com.dtforce.spring.kubernetes;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 @ConditionalOnProperty(name = "spring.kubernetes.discovery.enabled", matchIfMissing = true)
 public class KubernetesDiscoveryConfiguration
 {
 
 	@Bean
+	@ConditionalOnMissingBean
 	public KubernetesDiscoveryClient kubernetesDiscoveryClient()
 	{
 		return new KubernetesDiscoveryClient();
