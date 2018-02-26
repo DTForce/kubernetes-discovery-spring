7aa2395cbe554a5e831a0243f9cdf653eb75bbdf
Stéphane Lepin <stephane.lepin@dtforce.com>
2018-02-26 13:10:47 +0100

    Bugfix: unhandled exception if a Kube client call fails

--- src/main/java/com/dtforce/spring/kubernetes/KubernetesDiscoveryClient.java
+++ src/main/java/com/dtforce/spring/kubernetes/KubernetesDiscoveryClient.java
@@ -5,6 +5,7 @@ import io.fabric8.kubernetes.api.model.ServiceList;
 import io.fabric8.kubernetes.api.model.ServicePort;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
 import org.springframework.cloud.client.DefaultServiceInstance;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -38,46 +39,46 @@ public class KubernetesDiscoveryClient implements DiscoveryClient
 	@Override
 	public List<ServiceInstance> getInstances(String serviceId)
 	{
-		Service service = kubeClient.services().withName(serviceId).get();
-
-		if (service != null) {
-			// TODO: support multiple ports
-			ServicePort svcPort = service.getSpec().getPorts().get(0);
-			if (svcPort == null) {
-				return Collections.emptyList();
-			}
-
-			List<ServiceInstance> serviceInstances = new ArrayList<>();
-			serviceInstances.add(new DefaultServiceInstance(
-				service.getMetadata().getName(),
-				service.getSpec().getClusterIP(),
-				svcPort.getPort(),
-				false,
-				service.getMetadata().getAnnotations()
-			));
-			return serviceInstances;
+		Service service;
+		try {
+			service = kubeClient.services().withName(serviceId).get();
+		} catch(KubernetesClientException e) {
+			return Collections.emptyList();
 		}
 
-		return Collections.emptyList();
+		// TODO: support multiple ports
+		ServicePort svcPort = service.getSpec().getPorts().get(0);
+		if (svcPort == null) {
+			return Collections.emptyList();
+		}
+
+		List<ServiceInstance> serviceInstances = new ArrayList<>();
+		serviceInstances.add(new DefaultServiceInstance(
+			service.getMetadata().getName(),
+			service.getSpec().getClusterIP(),
+			svcPort.getPort(),
+			false,
+			service.getMetadata().getAnnotations()
+		));
+		return serviceInstances;
 	}
 
 	@Override
 	public List<String> getServices()
 	{
-		ServiceList serviceList = kubeClient.services().list();
-
-		if (serviceList != null) {
-			List<String> serviceNames = new ArrayList<>();
-
-			List<Service> items = serviceList.getItems();
-			for (Service svc : items) {
-				serviceNames.add(svc.getMetadata().getName());
-			}
-
-			return serviceNames;
+		ServiceList serviceList;
+		try {
+			serviceList = kubeClient.services().list();
+		} catch (KubernetesClientException e) {
+			return Collections.emptyList();
 		}
 
-		return Collections.emptyList();
+		List<String> serviceNames = new ArrayList<>();
+		List<Service> items = serviceList.getItems();
+		for (Service svc : items) {
+			serviceNames.add(svc.getMetadata().getName());
+		}
+		return serviceNames;
 	}
 
 }
